18654ad3f5dbbc8d3dc4094fcd1f815a34ce346c
NikolayBar <nikolbar@yandex.ru>
2024-11-07 20:16:06 +0300

    modul 14_L2

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from random import randint as rdi
 
 connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
@@ -16,10 +17,21 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
 
 # cursor.execute("INSERT INTO Users (username, email, ags) VALUES(?, ?, ?)", ("newuser", "ex@mail.ru", "28"))
 # for i in range(30):
-#     cursor.execute("INSERT INTO Users (username, email, ags) VALUES(?, ?, ?)", (f"{i}newuser", f"{i}ex@mail.ru", "28"))
+#     cursor.execute("INSERT INTO Users (username, email, ags) VALUES(?, ?, ?)",
+#                    (f"{i}newuser", f"{i}ex@mail.ru", f"{rdi(22, 60)}"))
 
 # cursor.execute("UPDATE Users SET ags=? WHERE username=?", (39, "newuser"))
 # cursor.execute("DELETE FROM Users WHERE username=?", ("newuser",))
+
+# cursor.execute("SELECT * FROM Users")
+# cursor.execute("SELECT username, ags FROM Users WHERE ags > ?", (29,))
+# cursor.execute("SELECT username, ags FROM Users GROUP BY ags")
+cursor.execute("SELECT username, ags FROM Users WHERE (ags > ? AND ags < ?)", (40, 50))
+
+users = cursor.fetchall()
+for user in users:
+    print(user)
+
 
 connection.commit()
 connection.close()
