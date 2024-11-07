7afaf53bb71646ae2128018fc9edb5e136c0f683
NikolayBar <nikolbar@yandex.ru>
2024-11-07 20:28:13 +0300

    modul 14_L3

--- db.py
+++ db.py
@@ -26,11 +26,13 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
 # cursor.execute("SELECT * FROM Users")
 # cursor.execute("SELECT username, ags FROM Users WHERE ags > ?", (29,))
 # cursor.execute("SELECT username, ags FROM Users GROUP BY ags")
-cursor.execute("SELECT username, ags FROM Users WHERE (ags > ? AND ags < ?)", (40, 50))
+# cursor.execute("SELECT username, ags FROM Users WHERE (ags > ? AND ags < ?)", (40, 50))
+a, b = 45, 50
+cursor.execute(f"SELECT username, ags FROM Users WHERE (ags > {a} AND ags < {b}) ORDER BY ags")
 
 users = cursor.fetchall()
 for user in users:
-    print(user)
+    print(*user, sep=': ')
 
 
 connection.commit()
