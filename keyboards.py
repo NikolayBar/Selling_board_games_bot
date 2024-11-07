2cf27f27d1b601fbc4562d0928b3d0fbc055634b
NikolayBar <nikolbar@yandex.ru>
2024-11-07 16:28:22 +0300

    part 2

--- keyboards.py
+++ keyboards.py
@@ -6,4 +6,17 @@ start_kb = ReplyKeyboardMarkup(keyboard=[
      KeyboardButton(text='О нас')]
 ], resize_keyboard=True)
 
+catalog_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Средняя игра', callback_data='medium')],
+        [InlineKeyboardButton(text='Большая игра', callback_data='big')],
+        [InlineKeyboardButton(text='Очень большая игра', callback_data='mega')],
+        [InlineKeyboardButton(text='Другие предложения', callback_data='other')]
+    ]
+)
 
+buy_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Купить!', url='http://ya.ru')]
+    ]
+)
--- main.py
+++ main.py
@@ -15,17 +15,45 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+
 @dp.message_handler(commands=['start'])
 async def start(msg_):
     await msg_.answer(t_start, reply_markup=start_kb)
+
 
 @dp.message_handler(text='О нас')
 async def price(msg_):
     await msg_.answer(t_about, reply_markup=start_kb)
 
-@dp.message_handler(text= 'Стоимость')
+
+@dp.message_handler(text='Стоимость')
 async def info(msg_):
     await msg_.answer('Что интересует?', reply_markup=catalog_kb)
+
+
+@dp.callback_query_handler(text='medium')
+async def buy_m(call):
+    await call.message.answer(text=t_M_game, reply_markup=buy_kb)
+    await call.answer()
+
+
+@dp.callback_query_handler(text='big')
+async def buy_l(call):
+    await call.message.answer(text=t_L_game, reply_markup=buy_kb)
+    await call.answer()
+
+
+@dp.callback_query_handler(text='mega')
+async def buy_xl(call):
+    await call.message.answer(text=t_XL_game, reply_markup=buy_kb)
+    await call.answer()
+
+
+@dp.callback_query_handler(text='other')
+async def buy_other(call):
+    await call.message.answer(text=t_other)
+    await call.answer()
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
